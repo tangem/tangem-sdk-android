eec5406e5a55ab64d1ee2d64f1edb4d25852a643
Tangem <git@tangem.com>
2020-07-22 09:32:08 +0300

    Updated on 2026-02-11

---
+++ tangem-core/src/main/java/com/tangem/commands/file/DeleteFileCommand.kt
@@ -0,0 +1,63 @@
+package com.tangem.commands.file
+
+import com.tangem.SessionEnvironment
+import com.tangem.TangemError
+import com.tangem.TangemSdkError
+import com.tangem.commands.Card
+import com.tangem.commands.CardStatus
+import com.tangem.commands.Command
+import com.tangem.commands.SimpleResponse
+import com.tangem.common.apdu.CommandApdu
+import com.tangem.common.apdu.Instruction
+import com.tangem.common.apdu.ResponseApdu
+import com.tangem.common.tlv.TlvBuilder
+import com.tangem.common.tlv.TlvDecoder
+import com.tangem.common.tlv.TlvTag
+
+
+typealias DeleteFileResponse = SimpleResponse
+
+
+class DeleteFileCommand(private val fileIndex: Int) : Command<DeleteFileResponse>() {
+
+    override val requiresPin2 = true
+
+    override fun performPreCheck(card: Card): TangemSdkError? {
+        if (card.status == CardStatus.NotPersonalized) {
+            return TangemSdkError.NotPersonalized()
+        }
+        if (card.isActivated) {
+            return TangemSdkError.NotActivated()
+        }
+        return null
+    }
+
+    override fun mapError(card: Card?, error: TangemError): TangemError {
+        if (error is TangemSdkError.InvalidParams) {
+            return TangemSdkError.Pin2OrCvcRequired()
+        }
+        return error
+    }
+
+    override fun serialize(environment: SessionEnvironment): CommandApdu {
+        val tlvBuilder = TlvBuilder()
+
+        tlvBuilder.append(TlvTag.CardId, environment.card?.cardId)
+        tlvBuilder.append(TlvTag.Pin, environment.pin1?.value)
+        tlvBuilder.append(TlvTag.Pin2, environment.pin2?.value)
+        tlvBuilder.append(TlvTag.WriteFileMode, FileDataMode.DeleteFile)
+        tlvBuilder.append(TlvTag.FileIndex, fileIndex)
+
+        return CommandApdu(Instruction.WriteFileData, tlvBuilder.serialize())
+    }
+
+    override fun deserialize(
+            environment: SessionEnvironment,
+            apdu: ResponseApdu
+    ): DeleteFileResponse {
+        val tlvData = apdu.getTlvData() ?: throw TangemSdkError.DeserializeApduFailed()
+
+        val decoder = TlvDecoder(tlvData)
+        return DeleteFileResponse(cardId = decoder.decode(TlvTag.CardId))
+    }
+}
