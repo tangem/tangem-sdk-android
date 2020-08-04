af4eef830c8ba934b4ccc0478f5cd816091e052c
Tangem <git@tangem.com>
2020-08-04 17:18:02 +0300

    Updated on 2026-04-11

--- tangem-sdk/src/main/java/com/tangem/tangem_sdk_new/extensions/Context.kt
+++ tangem-sdk/src/main/java/com/tangem/tangem_sdk_new/extensions/Context.kt
@@ -1,9 +1,15 @@
 package com.tangem.tangem_sdk_new.extensions
 
 import android.content.Context
+import android.os.Vibrator
 
 /**
 [REDACTED_AUTHOR]
  */
 fun Context.dpToPx(dp: Float): Float = dp * resources.displayMetrics.density
 fun Context.pxToDp(px: Float): Float = Math.round(px / resources.displayMetrics.density).toFloat()
+
+fun Context.vibrate(pattern: LongArray, repeat: Int = -1) {
+    val vibro = getSystemService(Context.VIBRATOR_SERVICE) as? Vibrator ?: return
+    vibro.vibrate(pattern, repeat)
+}
