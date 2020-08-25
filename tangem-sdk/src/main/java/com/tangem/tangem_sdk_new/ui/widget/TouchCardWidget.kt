36e84cc4d64c0616f6ecba3539cca17897a4e141
Tangem <git@tangem.com>
2020-08-25 22:45:34 +0300

    Updated on 2026-07-23

--- tangem-sdk/src/main/java/com/tangem/tangem_sdk_new/ui/widget/TouchCardWidget.kt
+++ tangem-sdk/src/main/java/com/tangem/tangem_sdk_new/ui/widget/TouchCardWidget.kt
@@ -3,9 +3,11 @@ package com.tangem.tangem_sdk_new.ui.widget
 import android.view.View
 import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.vectordrawable.graphics.drawable.VectorDrawableCompat
 import com.skyfishjy.library.RippleBackground
 import com.tangem.tangem_sdk_new.R
 import com.tangem.tangem_sdk_new.SessionViewDelegateState
+import com.tangem.tangem_sdk_new.extensions.setVectorDrawable
 import com.tangem.tangem_sdk_new.ui.TouchCardAnimation
 
 /**
@@ -16,6 +18,7 @@ class TouchCardWidget(mainView: View) : BaseSessionDelegateStateWidget(mainView)
     private val rippleBackgroundNfc = mainView.findViewById<RippleBackground>(R.id.rippleBackgroundNfc)
     private val ivHandCardHorizontal = mainView.findViewById<ImageView>(R.id.ivHandCardHorizontal)
     private val ivHandCardVertical = mainView.findViewById<ImageView>(R.id.ivHandCardVertical)
+    private val ivPhone = mainView.findViewById<ImageView>(R.id.ivPhone)
     private val llHand = mainView.findViewById<LinearLayout>(R.id.llHand)
     private val llNfc = mainView.findViewById<LinearLayout>(R.id.llNfc)
 
@@ -25,6 +28,10 @@ class TouchCardWidget(mainView: View) : BaseSessionDelegateStateWidget(mainView)
         nfcDeviceAntenna.init()
         nfcDeviceAntenna.onCardOnBack = { rippleBackgroundNfc.startRippleAnimation() }
         nfcDeviceAntenna.onCardMoveOut = { rippleBackgroundNfc.stopRippleAnimation() }
+
+        ivHandCardHorizontal.setVectorDrawable(R.drawable.hand_full_card_horizontal)
+        ivHandCardVertical.setVectorDrawable(R.drawable.hand_full_card_vertical)
+        ivPhone.setVectorDrawable(R.drawable.phone)
     }
 
     override fun setState(params: SessionViewDelegateState) {
