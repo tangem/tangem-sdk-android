57b61d74afdd27e8a42d9268c13de32e8fcb71e4
Tangem <git@tangem.com>
2020-08-25 10:33:46 +0300

    Updated on 2025-11-28

--- tangem-devkit/src/main/java/com/tangem/devkit/commons/GlobalFunctions.kt
+++
@@ -1,9 +0,0 @@
-package com.tangem.devkit.commons
-
-/**
-[REDACTED_AUTHOR]
- */
-fun <A, B> performAction(a: A?, b: B?, action: (A, B) -> Unit, onFail: (() -> Unit)? = null) {
-    if (a != null && b != null) action(a, b)
-    else onFail?.invoke()
-}
