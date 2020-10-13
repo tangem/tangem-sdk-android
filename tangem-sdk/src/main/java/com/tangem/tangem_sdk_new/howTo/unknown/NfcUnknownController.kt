cd13b4aef4994b627ad9b38a06e7969fbe12d230
Tangem <git@tangem.com>
2020-10-13 17:41:09 +0300

    Updated on 2026-02-11

---
+++ tangem-sdk/src/main/java/com/tangem/tangem_sdk_new/howTo/unknown/NfcUnknownController.kt
@@ -0,0 +1,45 @@
+package com.tangem.tangem_sdk_new.howTo.unknown
+
+import android.os.Build
+import android.os.VibrationEffect
+import android.os.Vibrator
+import com.tangem.tangem_sdk_new.howTo.HowToState
+import com.tangem.tangem_sdk_new.nfc.NfcManager
+import com.tangem.tangem_sdk_new.postUI
+
+/**
+[REDACTED_AUTHOR]
+ */
+class NfcUnknownController(
+    private val stateWidget: NfcUnknownWidget,
+    private val vibrator: Vibrator,
+    private val nfcManager: NfcManager,
+    private val repeatOnFinish: Boolean = false
+) {
+
+    init {
+        initTagDiscoveredCallback()
+    }
+
+    fun start() {
+        stateWidget.setState(HowToState.Unknown.FindAntenna)
+    }
+
+    fun onFinish(callback: () -> Unit) {
+        if (repeatOnFinish) {
+        } else {
+        }
+    }
+
+    private fun initTagDiscoveredCallback() {
+        nfcManager.onTagDiscovered = {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                vibrator.vibrate(VibrationEffect.createOneShot(100, VibrationEffect.DEFAULT_AMPLITUDE))
+            } else {
+                //deprecated in API 26
+                vibrator.vibrate(100)
+            }
+            postUI { stateWidget.setState(HowToState.Unknown.AntennaFound) }
+        }
+    }
+}
