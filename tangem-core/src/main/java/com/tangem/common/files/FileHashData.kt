e4c327e206fcf4c82d16ef3c921625cc2d06c271
Tangem <git@tangem.com>
2020-10-28 16:45:04 +0300

    Updated on 2026-04-11

---
+++ tangem-core/src/main/java/com/tangem/common/files/FileHashData.kt
@@ -0,0 +1,36 @@
+package com.tangem.common.files
+
+data class FileHashData(
+        val startingHash: ByteArray,
+        val finalizingHash: ByteArray,
+        val startingSignature: ByteArray? = null,
+        val finalizingSignature: ByteArray? = null
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as FileHashData
+
+        if (!startingHash.contentEquals(other.startingHash)) return false
+        if (!finalizingHash.contentEquals(other.finalizingHash)) return false
+        if (startingSignature != null) {
+            if (other.startingSignature == null) return false
+            if (!startingSignature.contentEquals(other.startingSignature)) return false
+        } else if (other.startingSignature != null) return false
+        if (finalizingSignature != null) {
+            if (other.finalizingSignature == null) return false
+            if (!finalizingSignature.contentEquals(other.finalizingSignature)) return false
+        } else if (other.finalizingSignature != null) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = startingHash.contentHashCode()
+        result = 31 * result + finalizingHash.contentHashCode()
+        result = 31 * result + (startingSignature?.contentHashCode() ?: 0)
+        result = 31 * result + (finalizingSignature?.contentHashCode() ?: 0)
+        return result
+    }
+}
