f752d76b882542d8e042a424be3f8c7ccb4726f2
Tangem <git@tangem.com>
2021-03-12 01:02:03 +0300

    Updated on 2026-04-11

--- tangem-core/src/main/java/com/tangem/FirmwareConstraints.kt
+++ tangem-core/src/main/java/com/tangem/FirmwareConstraints.kt
@@ -10,6 +10,7 @@ class FirmwareConstraints {
         val walletData = FirmwareVersion(4, 0)
         val pin2IsDefault = FirmwareVersion(4, 0)
         val files = FirmwareVersion(3, 29)
+        val pauseBeforePin2 = FirmwareVersion(1, 21)
     }
 
     object DeprecationVersions {
