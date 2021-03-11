5f2aa277210a31321c63afd60c065944e25e2b3a
Tangem <git@tangem.com>
2021-03-11 18:58:36 +0300

    Updated on 2026-04-11

--- tangem-sdk/src/main/java/com/tangem/tangem_sdk_new/ui/NfcEnableDialog.kt
+++ tangem-sdk/src/main/java/com/tangem/tangem_sdk_new/ui/NfcEnableDialog.kt
@@ -1,6 +1,7 @@
 package com.tangem.tangem_sdk_new.ui
 
 import android.app.Activity
+import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.provider.Settings
 import androidx.appcompat.app.AlertDialog
@@ -13,12 +14,18 @@ class NfcEnableDialog {
     fun show(activity: Activity) {
         val builder = AlertDialog.Builder(activity)
         builder.setCancelable(false)
-                .setIcon(R.drawable.ic_action_nfc_gray)
-                .setTitle(R.string.dialog_nfc_enable_title)
-                .setMessage(R.string.dialog_nfc_enable_text)
-                .setPositiveButton(R.string.common_ok)
-                { _, _ -> activity.startActivity(Intent(Settings.ACTION_NFC_SETTINGS)) }
-                .setNegativeButton(R.string.common_cancel) { dialog, _ -> dialog.cancel() }
+            .setIcon(R.drawable.ic_action_nfc_gray)
+            .setTitle(R.string.dialog_nfc_enable_title)
+            .setMessage(R.string.dialog_nfc_enable_text)
+            .setPositiveButton(R.string.common_ok)
+            { _, _ ->
+                try {
+                    activity.startActivity(Intent(Settings.ACTION_NFC_SETTINGS))
+                } catch (ex: ActivityNotFoundException) {
+                    print(ex.toString())
+                }
+            }
+            .setNegativeButton(R.string.common_cancel) { dialog, _ -> dialog.cancel() }
         dialog = builder.create()
         dialog?.show()
     }
