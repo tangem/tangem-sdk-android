c51cee652d79581697f986ec607b33efa206d3e7
Tangem <git@tangem.com>
2021-05-11 09:48:08 +0300

    Updated on 2026-07-23

--- tangem-tester-kotlin/src/main/java/com/tangem/tester/common/Extentions.kt
+++ tangem-tester-kotlin/src/main/java/com/tangem/tester/common/Extentions.kt
@@ -11,6 +11,22 @@ fun Int.foreach(block: (Int) -> Unit) {
     for (count in 0 until this) block(count)
 }
 
+fun Any?.isNumber(): Boolean {
+    if (this is Number) return true
+    if (this !is String) return false
+    if (this.isEmpty()) return false
+    this.forEach { if (!it.isDigit()) return false }
+    return true
+}
+
+fun String.insertAt(position: Int, what: String): String {
+    return this.replace("^(.{$position})", "$1$what");
+}
+
+fun String?.safeSplit(delimiter: String = "\\."): List<String>? {
+    return this?.split(delimiter.toRegex())
+}
+
 inline fun <reified T> MoshiJsonConverter.listFromJson(json: String): List<T> {
     return fromJson<List<T>>(json, typedList(T::class.java)) ?: listOf()
 }
