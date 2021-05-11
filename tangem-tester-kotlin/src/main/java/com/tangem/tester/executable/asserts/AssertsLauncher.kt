3eb3fa4f4f281b2375de24fb146bb9fd56cba817
Tangem <git@tangem.com>
2021-05-11 23:47:51 +0300

    Updated on 2025-11-28

--- tangem-tester-kotlin/src/main/java/com/tangem/tester/executable/asserts/AssertsLauncher.kt
+++ tangem-tester-kotlin/src/main/java/com/tangem/tester/executable/asserts/AssertsLauncher.kt
@@ -1,61 +1,35 @@
 package com.tangem.tester.executable.asserts
 
-import com.tangem.common.extensions.guard
-import com.tangem.tester.common.ExecutableError
+import com.tangem.tester.common.OnComplete
 import com.tangem.tester.common.TestResult
-import com.tangem.tester.executable.AssertHolder
-import com.tangem.tester.jsonModels.AssertModel
 import java.util.*
 
 /**
 [REDACTED_AUTHOR]
  */
 class AssertsLauncher(
-    private val parentStepName: String,
-    private val assertsQueue: Queue<AssertModel>,
-    private val assertHolder: AssertHolder,
+    private val assertsQueue: Queue<Assert>
 ) {
-    private lateinit var onAssertSequenceComplete: (TestResult) -> Unit
 
-    private val onAssertComplete: (TestResult) -> Unit = { result ->
-        when (result) {
-            is TestResult.Success -> {
-                if (assertsQueue.isEmpty()) {
-                    onAssertSequenceComplete(TestResult.Success())
-                } else {
-                    executeAssert(assertsQueue.poll())
-                }
-            }
-            is TestResult.Failure -> onAssertSequenceComplete(result)
-        }
-    }
-
-    fun run(callback: (TestResult) -> Unit) {
+    fun run(callback: OnComplete) {
         if (assertsQueue.isEmpty()) {
             callback(TestResult.Success())
             return
         }
-
-        onAssertSequenceComplete = callback
-        executeAssert(assertsQueue.poll())
+        executeAssert(assertsQueue.poll(), callback)
     }
 
-    private fun executeAssert(assertModel: AssertModel) {
-        val assert = assertHolder.getAssert(assertModel.type).guard {
-            onAssertComplete(TestResult.Failure(ExecutableError.ExecutableNotFoundError(assertModel.type)))
-            return
-        }
-
-        assert.init(assertModel)
-        assert.fetchVariables(parentStepName)?.let { error ->
-            onAssertComplete(TestResult.Failure(error))
-            return@let
-        }
-
+    private fun executeAssert(assert: Assert, callback: OnComplete) {
         assert.run { result ->
             when (result) {
-                is TestResult.Success -> onAssertComplete(TestResult.Success(result.name))
-                is TestResult.Failure -> onAssertComplete(result)
+                is TestResult.Success -> {
+                    if (assertsQueue.isEmpty()) {
+                        callback(TestResult.Success())
+                    } else {
+                        executeAssert(assertsQueue.poll(), callback)
+                    }
+                }
+                is TestResult.Failure -> callback(result)
             }
         }
     }
