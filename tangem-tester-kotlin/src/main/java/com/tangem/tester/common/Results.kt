52d196cedc6c53e76d4eeef734b6ce4d924203ed
Tangem <git@tangem.com>
2021-05-28 21:01:33 +0300

    Updated on 2026-02-11

--- tangem-tester-kotlin/src/main/java/com/tangem/tester/common/Results.kt
+++
@@ -1,9 +0,0 @@
-package com.tangem.tester.common
-
-/**
-[REDACTED_AUTHOR]
- */
-sealed class TestResult {
-    class Success(val name: String? = null) : TestResult()
-    class Failure(val error: TestFrameworkError) : TestResult()
-}
