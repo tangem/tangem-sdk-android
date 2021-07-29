f23b643200571118241a570cabf3eb3433507a43
Tangem <git@tangem.com>
2021-07-29 21:57:56 +0300

    Updated on 2026-06-16

--- tangem-sdk-android/src/main/java/com/tangem/tangem_sdk_new/extensions/TangemSdk.kt
+++ tangem-sdk-android/src/main/java/com/tangem/tangem_sdk_new/extensions/TangemSdk.kt
@@ -1,14 +1,19 @@
 package com.tangem.tangem_sdk_new.extensions
 
 import androidx.activity.ComponentActivity
+import com.tangem.Log
 import com.tangem.SessionViewDelegate
 import com.tangem.TangemSdk
+import com.tangem.TangemSdkLogger
 import com.tangem.common.core.Config
 import com.tangem.common.services.secure.SecureStorage
 import com.tangem.tangem_sdk_new.DefaultSessionViewDelegate
 import com.tangem.tangem_sdk_new.NfcLifecycleObserver
 import com.tangem.tangem_sdk_new.nfc.NfcManager
 import com.tangem.tangem_sdk_new.storage.create
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 fun TangemSdk.Companion.init(activity: ComponentActivity, config: Config = Config()): TangemSdk {
     val nfcManager = TangemSdk.initNfcManager(activity)
@@ -50,4 +55,25 @@ fun TangemSdk.Companion.initNfcManager(activity: ComponentActivity): NfcManager 
     nfcManager.setCurrentActivity(activity)
     activity.lifecycle.addObserver(NfcLifecycleObserver(nfcManager))
     return nfcManager
+}
+
+fun TangemSdk.Companion.createLogger(): TangemSdkLogger {
+    return object : TangemSdkLogger {
+        private val tag = "TangemSdkLogger"
+        private val dateFormatter: DateFormat = SimpleDateFormat("HH:mm:ss:SSS", Locale.getDefault())
+
+        override fun log(message: () -> String, level: Log.Level) {
+            if (!Log.Config.Verbose.levels.contains(level)) return
+
+            val prefixDelimiter = if (level.prefix.isEmpty()) "" else ": "
+            val logMessage = "${dateFormatter.format(Date())}: ${level.prefix}$prefixDelimiter${message()}"
+            when (level) {
+                Log.Level.Debug -> android.util.Log.d(tag, logMessage)
+                Log.Level.Info -> android.util.Log.i(tag, logMessage)
+                Log.Level.Warning -> android.util.Log.w(tag, logMessage)
+                Log.Level.Error -> android.util.Log.e(tag, logMessage)
+                else -> android.util.Log.v(tag, logMessage)
+            }
+        }
+    }
 }
