d91cf9bf66c4e24a9400cdabaee0a8cab6b5ea34
Tangem <git@tangem.com>
2021-08-30 18:56:39 +0300

    Updated on 2026-07-23

--- tangem-sdk-core/src/main/java/com/tangem/common/card/Card.kt
+++ tangem-sdk-core/src/main/java/com/tangem/common/card/Card.kt
@@ -85,6 +85,7 @@ data class Card internal constructor(
      *  User should withdraw the value to other blockchain wallet as soon as possible.
      *  Non-zero Health tag will also appear in responses of all other commands.
      */
+    @Transient
     internal var health: Int? = null,
 
     /**
