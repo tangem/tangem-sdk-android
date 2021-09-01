dc42437a1dc9f97aff0cd35af91d6647a48bd5f4
Tangem <git@tangem.com>
2021-09-01 21:25:56 +0300

    Updated on 2026-02-11

--- tangem-sdk-core/src/main/java/com/tangem/common/card/EllipticCurve.kt
+++ tangem-sdk-core/src/main/java/com/tangem/common/card/EllipticCurve.kt
@@ -1,18 +1,11 @@
 package com.tangem.common.card
 
-import com.google.gson.annotations.SerializedName
-
 /**
  * Elliptic curve used for wallet key operations.
  */
 enum class EllipticCurve(val curve: String) {
-    @SerializedName(value = "secp256k1")
     Secp256k1("secp256k1"),
-
-    @SerializedName(value = "secp256r1")
     Secp256r1("secp256r1"),
-
-    @SerializedName(value = "ed25519")
     Ed25519("ed25519");
 
     companion object {
