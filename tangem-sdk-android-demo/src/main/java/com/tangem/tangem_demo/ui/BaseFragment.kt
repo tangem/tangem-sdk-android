98d2c454bc3d6bf2c7a1ab79467117b12be3f3c8
Tangem <git@tangem.com>
2021-11-07 19:56:29 +0300

    Updated on 2026-04-11

--- tangem-sdk-android-demo/src/main/java/com/tangem/tangem_demo/ui/BaseFragment.kt
+++ tangem-sdk-android-demo/src/main/java/com/tangem/tangem_demo/ui/BaseFragment.kt
@@ -93,6 +93,7 @@ abstract class BaseFragment : Fragment() {
 
     protected fun createSdkConfig(): Config = Config().apply {
         linkedTerminal = false
+        allowUntrustedCards = true
         filter.allowedCardTypes = FirmwareVersion.FirmwareType.values().toList()
     }
 
