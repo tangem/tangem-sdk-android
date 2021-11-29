d788d5d6f8c1d2922a56a5502bd954babcdb659b
Tangem <git@tangem.com>
2021-11-30 02:01:49 +0300

    Updated on 2026-07-23

--- tangem-sdk-core/src/main/java/com/tangem/common/hdWallet/HDWalletError.kt
+++ tangem-sdk-core/src/main/java/com/tangem/common/hdWallet/HDWalletError.kt
@@ -7,6 +7,7 @@ sealed class HDWalletError : Exception() {
     object HardenedNotSupported : HDWalletError()
     object DerivationFailed : HDWalletError()
     object WrongPath : HDWalletError()
+    object WrongIndex : HDWalletError()
 
     override fun toString(): String = this::class.java.simpleName
 }
