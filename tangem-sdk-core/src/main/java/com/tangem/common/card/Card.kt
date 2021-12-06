0833093d0c270ea89d3ff6d21004ff18c6c25ca3
Tangem <git@tangem.com>
2021-12-06 13:46:56 +0300

    Updated on 2026-02-11

--- tangem-sdk-core/src/main/java/com/tangem/common/card/Card.kt
+++ tangem-sdk-core/src/main/java/com/tangem/common/card/Card.kt
@@ -87,11 +87,6 @@ data class Card internal constructor(
     val attestation: Attestation = Attestation.empty,
 
     /**
-     * Issuer's signature for backup process
-     */
-    val issuerSignature: ByteArray? = null,
-
-    /**
      *  Any non-zero value indicates that the card experiences some hardware problems.
      *  User should withdraw the value to other blockchain wallet as soon as possible.
      *  Non-zero Health tag will also appear in responses of all other commands.
