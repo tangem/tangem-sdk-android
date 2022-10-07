6f45c92b561346f9583022013d8db97bcaae45cb
Tangem <git@tangem.com>
2022-10-08 02:54:25 +0500

    Updated on 2026-02-11

--- tangem-sdk-core/src/main/java/com/tangem/common/card/WalletData.kt
+++ tangem-sdk-core/src/main/java/com/tangem/common/card/WalletData.kt
@@ -12,23 +12,24 @@ data class WalletData(
      * Token of the specified blockchain.
      */
     val token: Token?,
-)
+) {
 
-data class Token(
-    /**
-     * Display name of the token.
-     */
-    val name: String,
-    /**
-     * Token symbol
-     */
-    val symbol: String,
-    /**
-     * Smart contract address.
-     */
-    val contractAddress: String,
-    /**
-     * Number of decimals in token value.
-     */
-    val decimals: Int,
-)
+    data class Token(
+        /**
+         * Display name of the token.
+         */
+        val name: String,
+        /**
+         * Token symbol
+         */
+        val symbol: String,
+        /**
+         * Smart contract address.
+         */
+        val contractAddress: String,
+        /**
+         * Number of decimals in token value.
+         */
+        val decimals: Int,
+    )
+}
