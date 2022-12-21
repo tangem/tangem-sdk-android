e029096f5cbe84e767025238d182ba826450b4a8
Tangem <git@tangem.com>
2022-12-21 14:43:38 +0300

    Updated on 2026-01-28

--- tangem-sdk-core/src/main/java/com/tangem/common/CompletionResult.kt
+++ tangem-sdk-core/src/main/java/com/tangem/common/CompletionResult.kt
@@ -96,3 +96,28 @@ inline fun <T> CompletionResult<T>.doOnResult(
         .doOnFailure { block() }
         .doOnSuccess { block() }
 }
+
+@Suppress("RedundantUnitExpression")
+fun List<CompletionResult<Unit>>.fold(): CompletionResult<Unit> {
+    return fold(Unit) { _, _ -> Unit }
+}
+
+@Suppress("UNCHECKED_CAST", "RemoveRedundantQualifierName")
+inline fun <reified D, reified R> List<CompletionResult<D>>.fold(
+    initial: R,
+    operation: (acc: R, data: D) -> R,
+): CompletionResult<R> {
+    var resultData = initial
+    for (result in this) {
+        when (result) {
+            is CompletionResult.Success -> {
+                resultData = operation(resultData, result.data)
+            }
+            is CompletionResult.Failure -> {
+                return result as CompletionResult.Failure<R>
+            }
+        }
+    }
+
+    return CompletionResult.Success(resultData)
+}
