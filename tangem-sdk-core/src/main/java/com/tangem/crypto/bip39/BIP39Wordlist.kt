bf51e5ea42e184b586dbb40f119714b598e4b7c4
Tangem <git@tangem.com>
2023-03-29 11:22:18 +0300

    Updated on 2026-04-11

--- tangem-sdk-core/src/main/java/com/tangem/crypto/bip39/BIP39Wordlist.kt
+++ tangem-sdk-core/src/main/java/com/tangem/crypto/bip39/BIP39Wordlist.kt
@@ -2,6 +2,6 @@ package com.tangem.crypto.bip39
 
 class BIP39Wordlist : Wordlist {
 
-    override val wordlist: List<String>
+    override val words: List<String>
         get() = TODO("Not yet implemented")
 }
