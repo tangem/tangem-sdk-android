2f40a83d5bf1b4e819283e1ac2f8a8b92f5048a3
Tangem <git@tangem.com>
2023-03-29 11:22:18 +0300

    Updated on 2025-11-28

--- tangem-sdk-core/src/main/java/com/tangem/crypto/bip39/EntropyLength.kt
+++ tangem-sdk-core/src/main/java/com/tangem/crypto/bip39/EntropyLength.kt
@@ -21,6 +21,10 @@ sealed class EntropyLength(val count: Int) {
         return this.count / CHECKSUM_BITS_COUNT_DIVIDER
     }
 
+    fun toBytes(): Int {
+        return this.count / BYTE_SIZE
+    }
+
     companion object {
         private const val BITS_COUNT_128 = 128
         private const val BITS_COUNT_160 = 160
@@ -33,5 +37,30 @@ sealed class EntropyLength(val count: Int) {
         private const val WORDS_COUNT_FOR_BITS_224 = 21
         private const val WORDS_COUNT_FOR_BITS_256 = 24
         private const val CHECKSUM_BITS_COUNT_DIVIDER = 32
+        private const val BYTE_SIZE = 8
+
+        @kotlin.jvm.Throws(IllegalStateException::class)
+        fun create(length: Int): EntropyLength {
+            return when (length) {
+                WORDS_COUNT_FOR_BITS_128 -> Bits128Length
+                WORDS_COUNT_FOR_BITS_160 -> Bits160Length
+                WORDS_COUNT_FOR_BITS_192 -> Bits192Length
+                WORDS_COUNT_FOR_BITS_224 -> Bits224Length
+                WORDS_COUNT_FOR_BITS_256 -> Bits256Length
+                else -> error("no such EntropyLength")
+            }
+        }
+
+        @kotlin.jvm.Throws(IllegalStateException::class)
+        fun create(entropyData: ByteArray): EntropyLength {
+            return when (entropyData.count() * BYTE_SIZE) {
+                BITS_COUNT_128 -> Bits128Length
+                BITS_COUNT_160 -> Bits160Length
+                BITS_COUNT_192 -> Bits192Length
+                BITS_COUNT_224 -> Bits224Length
+                BITS_COUNT_256 -> Bits256Length
+                else -> error("no such EntropyLength")
+            }
+        }
     }
 }
