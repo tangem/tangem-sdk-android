fe3916b7ed905a40c54e4b4ca6202a7efa720736
Tangem <git@tangem.com>
2023-04-11 11:03:34 +0400

    Updated on 2026-01-28

--- tangem-sdk-core/src/main/java/com/tangem/crypto/hdWallet/bip32/ExtendedPublicKey.kt
+++ tangem-sdk-core/src/main/java/com/tangem/crypto/hdWallet/bip32/ExtendedPublicKey.kt
@@ -28,7 +28,7 @@ import com.tangem.operations.CommandResponse
 [REDACTED_AUTHOR]
  */
 @JsonClass(generateAdapter = true)
-class ExtendedPublicKey @kotlin.jvm.Throws constructor(
+class ExtendedPublicKey @Throws constructor(
     val publicKey: ByteArray,
     val chainCode: ByteArray,
     val depth: Int = 0,
--- tangem-sdk-core/src/main/java/com/tangem/crypto/hdWallet/bip32/serialization/ExtendedKeySerializer.kt
+++ tangem-sdk-core/src/main/java/com/tangem/crypto/hdWallet/bip32/serialization/ExtendedKeySerializer.kt
@@ -6,16 +6,19 @@ class ExtendedKeySerializer {
     enum class Version {
         Public, Private;
 
+        // https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki#serialization-format
+        // Because of the choice of the version bytes,
+        // the Base58 representation will start with "xprv" or "xpub" on mainnet, "tprv" or "tpub" on testnet
         @Suppress("MagicNumber")
         fun getPrefix(networkType: NetworkType): Long {
             return when (this) {
                 Public -> when (networkType) {
-                    NetworkType.Mainnet -> 0x0488b21eL
-                    NetworkType.Testnet -> 0x043587cfL
+                    NetworkType.Mainnet -> 0x0488b21eL // xpub
+                    NetworkType.Testnet -> 0x043587cfL // tpub
                 }
                 Private -> when (networkType) {
-                    NetworkType.Mainnet -> 0x0488ADE4L
-                    NetworkType.Testnet -> 0x04358394L
+                    NetworkType.Mainnet -> 0x0488ADE4L // xprv
+                    NetworkType.Testnet -> 0x04358394L // tprv
                 }
             }
         }
