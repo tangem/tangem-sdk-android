314cef4d86c222978cacbaa389efdbe06680aaef
Tangem <git@tangem.com>
2023-05-17 17:32:49 +0300

    Updated on 2026-06-16

--- tangem-sdk-android/src/main/java/com/tangem/sdk/ui/widget/TouchCardWidget.kt
+++ tangem-sdk-android/src/main/java/com/tangem/sdk/ui/widget/TouchCardWidget.kt
@@ -1,16 +1,22 @@
 package com.tangem.sdk.ui.widget
 
 import android.animation.Animator
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.view.View
+import android.view.ViewGroup
 import android.view.ViewTreeObserver
 import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatImageView
 import com.skyfishjy.library.RippleBackground
+import com.tangem.common.core.ScanTagImage
 import com.tangem.sdk.R
 import com.tangem.sdk.SessionViewDelegateState
 import com.tangem.sdk.extensions.dpToPx
 import com.tangem.sdk.extensions.fadeOut
 import com.tangem.sdk.extensions.hide
 import com.tangem.sdk.extensions.isOnTheBack
+import com.tangem.sdk.extensions.show
 import com.tangem.sdk.ui.NfcLocation
 import com.tangem.sdk.ui.animation.AnimationProperty
 import com.tangem.sdk.ui.animation.TapAnimationCallback
@@ -23,7 +29,12 @@ import com.tangem.sdk.ui.animation.TouchCardAnimation.Companion.calculateRelativ
 class TouchCardWidget(
     mainView: View,
     private val nfcLocation: NfcLocation,
+    private var scanImage: ScanTagImage = ScanTagImage.GenericCard,
 ) : BaseSessionDelegateStateWidget(mainView) {
+
+    private val genericImageContainer = mainView.findViewById<ViewGroup>(R.id.clGenericImage)
+    private val customImageContainer = mainView.findViewById<ViewGroup>(R.id.flCustomImage)
+    private val customImageView = mainView.findViewById<AppCompatImageView>(R.id.customImage)
 
     private val rippleBackgroundNfc = mainView.findViewById<RippleBackground>(R.id.rippleBackgroundNfc)
     private val ivHandCardHorizontal = mainView.findViewById<ImageView>(R.id.ivHandCardHorizontal)
@@ -38,16 +49,60 @@ class TouchCardWidget(
         nfcLocation,
     )
 
+    private var customBitmapHolder: BitmapImageHolder? = null
+
     init {
         rippleBackgroundNfc.alpha = 0f
     }
 
     override fun setState(params: SessionViewDelegateState) {
-        when (params) {
-            is SessionViewDelegateState.Ready -> animate()
-            is SessionViewDelegateState.TagLost -> animate()
-            else -> stopAnimation()
+        when (val scanTagImage = scanImage) {
+            ScanTagImage.GenericCard -> {
+                when (params) {
+                    is SessionViewDelegateState.Ready -> animate()
+                    is SessionViewDelegateState.TagLost -> animate()
+                    else -> stopAnimation()
+                }
+            }
+            is ScanTagImage.Image -> {
+                var holder = customBitmapHolder
+                if (holder == null) {
+                    holder = BitmapImageHolder(scanTagImage.bitmapArray)
+                } else if (!holder.bitmapArray.contentEquals(scanTagImage.bitmapArray)) {
+                    holder.bitmap.recycle()
+                    holder = BitmapImageHolder(scanTagImage.bitmapArray)
+                }
+
+                if (scanTagImage.verticalOffset != customImageView.paddingTop) {
+                    customImageView.setPadding(
+                        customImageView.paddingStart,
+                        scanTagImage.verticalOffset,
+                        customImageView.paddingEnd,
+                        customImageView.paddingBottom,
+                    )
+                }
+                customImageView.setImageBitmap(holder.bitmap)
+                customBitmapHolder = holder
+            }
         }
+        switchScanTagImageVisibility()
+    }
+
+    private fun switchScanTagImageVisibility() {
+        when (scanImage) {
+            ScanTagImage.GenericCard -> {
+                customImageContainer.hide()
+                genericImageContainer.show()
+            }
+            is ScanTagImage.Image -> {
+                genericImageContainer.hide()
+                customImageContainer.show()
+            }
+        }
+    }
+
+    fun setScanImage(scanImage: ScanTagImage) {
+        this.scanImage = scanImage
     }
 
     private fun animate() {
@@ -96,5 +151,11 @@ class TouchCardWidget(
 
     override fun onBottomSheetDismiss() {
         stopAnimation()
+        customBitmapHolder?.bitmap?.recycle()
     }
+
+    private data class BitmapImageHolder(
+        val bitmapArray: ByteArray,
+        val bitmap: Bitmap = BitmapFactory.decodeByteArray(bitmapArray, 0, bitmapArray.size, BitmapFactory.Options()),
+    )
 }
