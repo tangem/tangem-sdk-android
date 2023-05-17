f156686779b03c6b9c86b491c910a6c00f97a84e
Tangem <git@tangem.com>
2023-05-17 10:58:44 +0400

    Updated on 2026-07-23

--- tangem-sdk-core/src/main/java/com/tangem/common/card/EllipticCurve.kt
+++ tangem-sdk-core/src/main/java/com/tangem/common/card/EllipticCurve.kt
@@ -7,6 +7,10 @@ enum class EllipticCurve(val curve: String) {
     Secp256k1("secp256k1"),
     Secp256r1("secp256r1"),
     Ed25519("ed25519"),
+    Bls12381G2("bls12381_G2"),
+    Bls12381G2Aug("bls12381_G2_AUG"),
+    Bls12381G2Pop("bls12381_G2_POP"),
+    Bip0340("bip0340"),
     ;
 
     companion object {
