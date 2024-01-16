f646df0e870893032d186ef2d30a14903f44a838
Tangem <git@tangem.com>
2024-01-16 23:11:21 +0300

    Updated on 2026-01-28

--- tangem-sdk-core/src/main/java/com/tangem/operations/backup/LinkPrimaryCardCommand.kt
+++ tangem-sdk-core/src/main/java/com/tangem/operations/backup/LinkPrimaryCardCommand.kt
@@ -108,7 +108,7 @@ class LinkPrimaryCardCommand(
         tlvBuilder.append(TlvTag.Pin, environment.accessCode.value)
         tlvBuilder.append(TlvTag.Pin2, environment.passcode.value)
         tlvBuilder.append(TlvTag.PrimaryCardLinkingKey, primaryCard.linkingKey)
-        tlvBuilder.append(TlvTag.Certificate, primaryCard.generateCertificate())
+        tlvBuilder.append(TlvTag.Certificate, primaryCard.certificate)
         tlvBuilder.append(TlvTag.BackupAttestSignature, attestSignature)
         tlvBuilder.append(TlvTag.NewPin, accessCode)
         tlvBuilder.append(TlvTag.NewPin2, passcode)
