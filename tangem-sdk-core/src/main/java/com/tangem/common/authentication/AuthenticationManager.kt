89c96fa752166fea89389d660c0dc200e67ddbf8
Tangem <git@tangem.com>
2024-03-18 11:29:14 +0400

    Updated on 2026-01-28

--- tangem-sdk-core/src/main/java/com/tangem/common/authentication/AuthenticationManager.kt
+++ tangem-sdk-core/src/main/java/com/tangem/common/authentication/AuthenticationManager.kt
@@ -1,6 +1,7 @@
 package com.tangem.common.authentication
 
 import com.tangem.common.core.TangemSdkError
+import javax.crypto.Cipher
 import kotlin.time.Duration
 
 /**
@@ -26,19 +27,33 @@ interface AuthenticationManager {
      *
      * @param params [AuthenticationParams] that specify the authentication process.
      *
+     * @return [AuthenticationResult] that contains the successful result of the authentication process.
+     *
+     * @throws TangemSdkError.AuthenticationAlreadyInProgress if another authentication process is already in progress.
      * @throws TangemSdkError.AuthenticationUnavailable if authentication is unavailable.
      * @throws TangemSdkError.AuthenticationCanceled if the authentication was cancelled.
      * @throws TangemSdkError.AuthenticationFailed if authentication fails for any other reason.
      */
-    suspend fun authenticate(params: AuthenticationParams)
+    suspend fun authenticate(params: AuthenticationParams): AuthenticationResult
 
     /**
      * Parameters that specify the authentication process.
      *
+     * @property cipher The cipher that will be checked with authentication and can be used to encrypt or decrypt data.
      * @property timeout The maximum time to wait for the authentication to complete. If it's reached, the
      * authentication process will be cancelled.
      */
     interface AuthenticationParams {
+        val cipher: Cipher?
         val timeout: Duration?
+    }
+
+    /**
+     * The successful result of the authentication process.
+     *
+     * @property cipher The cipher that can be used to encrypt or decrypt data.
+     */
+    interface AuthenticationResult {
+        val cipher: Cipher?
     }
 }
