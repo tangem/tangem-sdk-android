54d147a9614063066d3bf53125850cf44727e3e9
Tangem <git@tangem.com>
2024-05-06 12:06:15 +0400

    Updated on 2025-11-28

--- tangem-sdk-core/src/main/java/com/tangem/common/authentication/keystore/DummyKeystoreManager.kt
+++ tangem-sdk-core/src/main/java/com/tangem/common/authentication/keystore/DummyKeystoreManager.kt
@@ -4,11 +4,16 @@ import javax.crypto.SecretKey
 
 class DummyKeystoreManager : KeystoreManager {
 
-    override suspend fun get(masterKeyConfig: KeystoreManager.MasterKeyConfig, keyAlias: String): SecretKey? = null
+    override suspend fun get(
+        masterKeyConfig: KeystoreManager.MasterKeyConfig,
+        keyAlias: String,
+        forceAuthentication: Boolean,
+    ): SecretKey? = null
 
     override suspend fun get(
         masterKeyConfig: KeystoreManager.MasterKeyConfig,
         keyAliases: Set<String>,
+        forceAuthentication: Boolean,
     ): Map<String, SecretKey> = emptyMap()
 
     override suspend fun store(masterKeyConfig: KeystoreManager.MasterKeyConfig, keyAlias: String, key: SecretKey) =
