792f4894b639449d520ce11eceff56493fb1ec85
Tangem <git@tangem.com>
2024-05-28 13:19:52 +0800

    Updated on 2026-06-16

--- tangem-sdk-core/src/main/java/com/tangem/operations/personalization/PersonalizeCommand.kt
+++ tangem-sdk-core/src/main/java/com/tangem/operations/personalization/PersonalizeCommand.kt
@@ -55,7 +55,7 @@ class PersonalizeCommand(
     override fun run(session: CardSession, callback: CompletionCallback<Card>) {
         Log.command(this)
         // We have to run preflight read ourselves to catch the notPersonalized error
-        val read = PreflightReadTask(PreflightReadMode.ReadCardOnly, null)
+        val read = PreflightReadTask(PreflightReadMode.ReadCardOnly)
         read.run(session) { result ->
             when (result) {
                 is CompletionResult.Success -> callback(CompletionResult.Failure(TangemSdkError.AlreadyPersonalized()))
