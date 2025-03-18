b935d2cf0b541e6cd164e4d61f7674183a3320a7
Tangem <git@tangem.com>
2025-03-18 10:31:24 +0400

    Updated on 2026-07-23

--- tangem-sdk-core/src/main/java/com/tangem/common/services/secure/SecureStorage.kt
+++ tangem-sdk-core/src/main/java/com/tangem/common/services/secure/SecureStorage.kt
@@ -4,11 +4,18 @@ package com.tangem.common.services.secure
 [REDACTED_AUTHOR]
  */
 interface SecureStorage {
+
     fun get(account: String): ByteArray?
-    fun store(data: ByteArray, account: String, overwrite: Boolean = true)
+
+    fun getAsString(key: String): String?
+
+    fun store(key: String, value: String)
+
+    fun store(data: ByteArray, account: String)
+
     fun delete(account: String)
+
     fun storeKey(key: ByteArray, account: String)
-    fun readKey(account: String): ByteArray?
 
     companion object
 }
