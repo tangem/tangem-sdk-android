36110cee21864567fee594b5d9862fd5bde1bf05
Tangem <git@tangem.com>
2025-03-25 17:07:04 +0300

    Updated on 2026-06-16

--- tangem-sdk-core/src/main/java/com/tangem/operations/attestation/api/BaseUrl.kt
+++ tangem-sdk-core/src/main/java/com/tangem/operations/attestation/api/BaseUrl.kt
@@ -1,6 +1,6 @@
 package com.tangem.operations.attestation.api
 
 internal enum class BaseUrl(val url: String) {
-    CARD_DATA("https://api.tangem-tech.com/"),
+    CARD_DATA("https://api.tangem.org/"),
     VERIFY("https://verify.tangem.com/"),
 }
