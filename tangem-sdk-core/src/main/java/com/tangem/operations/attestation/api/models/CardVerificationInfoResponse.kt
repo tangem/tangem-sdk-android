4a365d2be632c2a22032d1c8face9b0153bb67ab
Tangem <git@tangem.com>
2025-04-03 13:20:58 +0400

    Updated on 2026-04-11

--- tangem-sdk-core/src/main/java/com/tangem/operations/attestation/api/models/CardVerificationInfoResponse.kt
+++ tangem-sdk-core/src/main/java/com/tangem/operations/attestation/api/models/CardVerificationInfoResponse.kt
@@ -3,6 +3,12 @@ package com.tangem.operations.attestation.api.models
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
+/**
+ * Card verification info response
+ *
+ * @property manufacturerSignature issuer certificate
+ * @property issuerSignature       card certificate
+ */
 @JsonClass(generateAdapter = true)
 internal data class CardVerificationInfoResponse(
     @Json(name = "manufacturer_signature") val manufacturerSignature: String?,
