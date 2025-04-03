24fc1836362c6a9f9e0d6977da8bbd52d3548435
Tangem <git@tangem.com>
2025-04-03 13:20:58 +0400

    Updated on 2026-07-23

---
+++ tangem-sdk-core/src/main/java/com/tangem/operations/attestation/service/CommonOnlineAttestationService.kt
@@ -0,0 +1,68 @@
+package com.tangem.operations.attestation.service
+
+import com.tangem.common.CompletionResult
+import com.tangem.common.core.TangemSdkError
+import com.tangem.common.json.MoshiJsonConverter
+import com.tangem.common.services.CardVerificationInfoStore
+import com.tangem.common.services.Result
+import com.tangem.common.services.secure.SecureStorage
+import com.tangem.common.services.toTangemSdkError
+import com.tangem.operations.attestation.api.TangemApiService
+import com.tangem.operations.attestation.api.models.CardVerificationInfoResponse
+import com.tangem.operations.attestation.verification.OnlineAttestationVerifier
+
+internal class CommonOnlineAttestationService(
+    private val tangemApiService: TangemApiService,
+    private val onlineAttestationVerifier: OnlineAttestationVerifier,
+    private val cardVerificationInfoStore: CardVerificationInfoStore,
+) : OnlineAttestationService {
+
+    constructor(
+        tangemApiService: TangemApiService,
+        onlineAttestationVerifier: OnlineAttestationVerifier,
+        secureStorage: SecureStorage,
+    ) : this(tangemApiService, onlineAttestationVerifier, createDefaultStore(secureStorage))
+
+    override suspend fun attestCard(
+        cardId: String,
+        cardPublicKey: ByteArray,
+    ): CompletionResult<CardVerificationInfoResponse> {
+        return when (val result = getAttestationData(cardId, cardPublicKey)) {
+            is CompletionResult.Success -> {
+                val isVerified = onlineAttestationVerifier.verify(response = result.data)
+
+                if (isVerified) {
+                    cardVerificationInfoStore.store(cardPublicKey = cardPublicKey, response = result.data)
+                    result
+                } else {
+                    CompletionResult.Failure(TangemSdkError.CardVerificationFailed())
+                }
+            }
+            is CompletionResult.Failure -> CompletionResult.Failure(TangemSdkError.CardVerificationFailed())
+        }
+    }
+
+    private suspend fun getAttestationData(
+        cardId: String,
+        cardPublicKey: ByteArray,
+    ): CompletionResult<CardVerificationInfoResponse> {
+        val cache = cardVerificationInfoStore.get(cardPublicKey)
+
+        if (cache != null) return CompletionResult.Success(data = cache)
+
+        return when (val result = tangemApiService.getOnlineAttestationResponse(cardId, cardPublicKey)) {
+            is Result.Failure -> CompletionResult.Failure(error = result.toTangemSdkError())
+            is Result.Success -> CompletionResult.Success(data = result.data)
+        }
+    }
+
+    private companion object {
+
+        fun createDefaultStore(secureStorage: SecureStorage): CardVerificationInfoStore {
+            return CardVerificationInfoStore(
+                storage = secureStorage,
+                moshi = MoshiJsonConverter.INSTANCE.moshi,
+            )
+        }
+    }
+}
---
+++ tangem-sdk-core/src/main/java/com/tangem/operations/attestation/verification/ManufacturerPublicKeyProvider.kt
@@ -0,0 +1,26 @@
+package com.tangem.operations.attestation.verification
+
+import com.tangem.common.card.FirmwareVersion
+
+/**
+ * Provider of [ManufacturerPublicKey]
+ *
+ * @property firmwareVersion  firmware version
+ * @property manufacturerName manufacturer name
+ *
+[REDACTED_AUTHOR]
+ */
+class ManufacturerPublicKeyProvider(
+    private val firmwareVersion: FirmwareVersion,
+    private val manufacturerName: String,
+) {
+
+    fun get(): ManufacturerPublicKey? {
+        if (firmwareVersion < FirmwareVersion.KeysImportAvailable) return ManufacturerPublicKey.Tangem
+
+        val publicKey = ManufacturerPublicKey.values().firstOrNull { manufacturerName == it.id }
+        if (publicKey == null) return null
+
+        return publicKey
+    }
+}
