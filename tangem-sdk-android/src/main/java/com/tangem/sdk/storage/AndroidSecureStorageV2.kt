33db976ec779163eba235be44fd81f5ba93554c8
Tangem <git@tangem.com>
2025-07-22 20:14:07 +0300

    Updated on 2026-02-11

--- tangem-sdk-android/src/main/java/com/tangem/sdk/storage/AndroidSecureStorageV2.kt
+++ tangem-sdk-android/src/main/java/com/tangem/sdk/storage/AndroidSecureStorageV2.kt
@@ -6,6 +6,7 @@ import android.os.Build
 import android.security.keystore.KeyGenParameterSpec
 import android.security.keystore.KeyProperties
 import android.util.Base64
+import androidx.annotation.RequiresApi
 import com.tangem.common.services.secure.SecureStorage
 import java.nio.ByteBuffer
 import java.security.KeyStore
@@ -15,13 +16,15 @@ import javax.crypto.SecretKey
 import javax.crypto.spec.GCMParameterSpec
 import androidx.core.content.edit
 
-private const val AES_KEY_ALIAS = "AndroidSecureStorageV2_aes_key"
+private const val AES_KEY_ALIAS_STRONG_BOX = "AndroidSecureStorageV2_aes_key"
+private const val AES_KEY_ALIAS_HARDWARE = "AndroidSecureStorageV2_aes_key_hardware"
 private const val ALGORITHM = "AES/GCM/NoPadding"
 private const val IV_TAG_LENGTH_BIT = 128
 private const val KEY_SIZE = 256
 
 class AndroidSecureStorageV2(
     private val appContext: Context,
+    private val useStrongBox: Boolean,
     name: String = "AndroidSecureStorageV2",
 ) : SecureStorage {
 
@@ -102,10 +105,10 @@ class AndroidSecureStorageV2(
 
     private fun getAesKey(): SecretKey {
         val keyStore = KeyStore.getInstance("AndroidKeyStore").apply { load(null) }
-        return if (keyStore.containsAlias(AES_KEY_ALIAS).not()) {
+        return if (keyStore.containsAlias(getAesKeyAlias()).not()) {
             generateAesKey()
         } else {
-            (keyStore.getEntry(AES_KEY_ALIAS, null) as KeyStore.SecretKeyEntry).secretKey
+            (keyStore.getEntry(getAesKeyAlias(), null) as KeyStore.SecretKeyEntry).secretKey
         }
     }
 
@@ -113,26 +116,35 @@ class AndroidSecureStorageV2(
         val keyGenerator =
             KeyGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore")
         val keyGenParameterSpec = KeyGenParameterSpec.Builder(
-            AES_KEY_ALIAS,
+            getAesKeyAlias(),
             KeyProperties.PURPOSE_ENCRYPT or KeyProperties.PURPOSE_DECRYPT,
         ).apply {
             setBlockModes(KeyProperties.BLOCK_MODE_GCM)
             setEncryptionPaddings(KeyProperties.ENCRYPTION_PADDING_NONE)
             setKeySize(KEY_SIZE)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                setIsStrongBoxBacked(hasStrongBox())
+            if (useStrongBoxFeature()) {
+                setIsStrongBoxBacked(true)
             }
         }.build()
         keyGenerator.init(keyGenParameterSpec)
         return keyGenerator.generateKey()
     }
 
-    private fun hasStrongBox(): Boolean {
-        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            appContext.packageManager
-                .hasSystemFeature(PackageManager.FEATURE_STRONGBOX_KEYSTORE)
+    private fun getAesKeyAlias(): String {
+        return if (useStrongBoxFeature()) {
+            AES_KEY_ALIAS_STRONG_BOX
         } else {
-            false
+            AES_KEY_ALIAS_HARDWARE
         }
+    }
+
+    private fun useStrongBoxFeature(): Boolean {
+        return useStrongBox && Build.VERSION.SDK_INT >= Build.VERSION_CODES.P && hasStrongBox()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    private fun hasStrongBox(): Boolean {
+        return appContext.packageManager
+            .hasSystemFeature(PackageManager.FEATURE_STRONGBOX_KEYSTORE)
     }
 }
